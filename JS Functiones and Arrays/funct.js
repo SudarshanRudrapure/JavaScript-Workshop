d45d388a09ca7a16168d28bdc82cd9292cb74cf2
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-04-14 10:21:54 +0530

    JavaScript Arrays and Functiones

---
+++ JS Functiones and Arrays/funct.js
@@ -0,0 +1,16 @@
+/*function dish(){
+    console.log("1. put on heat")
+    console.log("2. Add Panner")
+    console.log("3. Boil")
+    console.log("4. Tadka")
+}
+dish()
+*/
+
+function dish(ingredient){
+    console.log("1. put on heat")
+    console.log("2. Add "+ingredient)
+    console.log("3. Boil")
+    console.log("4. Tadka")
+}
+dish("panner")
