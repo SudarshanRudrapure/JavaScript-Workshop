96aa235d919ec156ddfa91936f6f2bcd987edc75
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-04-15 10:36:16 +0530

    Basics

--- JS Functiones/assignment -02/script.js
+++
@@ -1,8 +0,0 @@
-//use item(todos)  printing  each value in array
-
-let todos=["Learn JS", "Do Assignment", "Take Break"]
-function fun(todos){
-    todos.forEach((item)=> console.log(item));
-}
-fun(todos);
-
